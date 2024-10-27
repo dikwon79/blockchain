61489dc47fa238aa3405fb9246ae3b46f403bd7e
Dong il Kwon <30903150+dikwon79@users.noreply.github.com>
2024-10-27 18:10:27 +0900

    chain

--- src/index.ts
+++ src/index.ts
@@ -23,3 +23,31 @@ class Block implements BlockShape{
     }
 
 }
+
+
+class Blockchain{
+    private blocks: Block[];
+    constructor(){
+        this.blocks = [];
+    }
+    private getPrevHash(){
+        if(this.blocks.length === 0) return "";
+        return this.blocks[this.blocks.length - 1].hash;
+    }
+    public addBlock(data: string){
+        const newBlock = new Block(this.getPrevHash(), this.blocks.length + 1, data)
+        this.blocks.push(newBlock);
+    }
+    public getBlocks(){
+        return [...this.blocks];  // 배열로 해야 핵킹을 막을수 있다. 블록체인 state와 연결되지가 않았다.
+    }
+}
+
+const blockchain = new Blockchain();
+
+blockchain.addBlock("First one");
+blockchain.addBlock("Second one");
+blockchain.addBlock("Third one");
+
+blockchain.getBlocks().push(new Block("xxxx",11111,"HACKEDDDDD"))
+console.log(blockchain.getBlocks());
